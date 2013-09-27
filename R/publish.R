245d6e16d4b10237a681f848d408fde35586a3cc
JJ Allaire <jj@rstudio.org>
2013-09-27 12:15:46 -0400

    initial commit

---
+++ R/publish.R
@@ -0,0 +1,11 @@
+
+#' Publish an application to shinyapps.io
+#' 
+#' Publish an application to shinyapps.io
+#' @param appDir directory of application to publish 
+#' @export
+publish <- function(appDir = ".") {
+  
+}
+
+
