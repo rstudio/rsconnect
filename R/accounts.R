95adf5e8cef01b24c8af560bb1ae6939781e5c2e
JJ Allaire <jj@rstudio.org>
2013-09-29 14:36:28 -0400

    config dir

--- R/accounts.R
+++ R/accounts.R
@@ -11,3 +11,10 @@ accounts <- function() {
 removeAccount <- function() {
   
 }
+
+accountsConfigDir <- function() {
+  configDir <- file.path(shinyappsConfigDir(), "accounts")
+  if (!file.exists(configDir))
+    dir.create(configDir, recursive=TRUE)
+  configDir
+}
---
+++ R/config.R
@@ -0,0 +1,32 @@
+
+
+shinyappsConfigDir <- function() {
+  applicationConfigDir("shinyapps")
+}
+
+applicationConfigDir <- function(appName) {
+  
+  # get the home directory from the operating system (in case
+  # the user has redefined the meaning of ~) but fault back 
+  # to ~ if there is no HOME variable defined
+  homeDir <- Sys.getenv("HOME", unset="~")
+  
+  # determine application config dir (platform specific)
+  sysName <- Sys.info()[['sysname']]
+  if (identical(sysName, "Windows"))
+    configDir <- Sys.getenv("APPDATA")
+  else if (identical(sysName, "Darwin"))
+    configDir <- file.path(homeDir, "Library/Application Support")
+  else
+    configDir <- Sys.getenv("XDG_CONFIG_HOME", file.path(homeDir, ".config"))
+  
+  # append the application name
+  configDir <- file.path(configDir, "R", appName)
+  
+  # ensure that it exists
+  if (!file.exists(configDir))
+    dir.create(configDir, recursive=TRUE)
+  
+  # return it
+  configDir
+}
