006d14746087050da156f441467a8afd7f029209
JJ Allaire <jj@rstudio.org>
2013-09-30 09:08:59 -0400

    account management functions

--- R/accounts.R
+++ R/accounts.R
@@ -1,15 +1,54 @@
 
 
+#' Account Management Functions
+#' 
+#' Functions to add, enumerate, and remove ShinyApps accounts on the local
+#' system.
+#' @rdname accounts
+#' @export
+accounts <- function() {
+  tools::file_path_sans_ext(list.files(accountsConfigDir(), 
+                                       pattern=glob2rx("*.dcf")))
+}
+
+
+#' @rdname accounts
+#' @export
 setAccountInfo <- function(name, key, secret) {
   
+  # TODO: validate inputs
+  
+  write.dcf(list(name = name, key = key, secret = secret), 
+            accountConfigFile(name))
 }
 
-accounts <- function() {
+#' @rdname accounts
+#' @export
+accountInfo <- function(name) {
   
+  configFile <- accountConfigFile(name)
+  if (!file.exists(configFile))
+    stop("Account named '", name, "' does not exist")
+  
+  accountDcf <- read.dcf(accountConfigFile(name), all=TRUE)
+  as.list(accountDcf)
 }
 
-removeAccount <- function() {
+
+#' @rdname accounts
+#' @export
+removeAccount <- function(name) {
+  configFile <- accountConfigFile(name)
+  if (!file.exists(configFile))
+    stop("Account named '", name, "' does not exist")
   
+  invisible(file.remove(configFile))
+}
+
+
+accountConfigFile <- function(name) {
+  normalizePath(file.path(accountsConfigDir(), paste(name, ".dcf", sep="")),
+                mustWork = FALSE)
 }
 
 accountsConfigDir <- function() {
