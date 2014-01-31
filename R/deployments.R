a1d874ffd21300ac557359481bbaea7a788e7f83
Jonathan McPherson <jonathan@rstudio.com>
2014-01-31 11:12:17 -0800

    expose and document readDeployments function

--- R/deployments.R
+++ R/deployments.R
@@ -7,6 +7,22 @@ saveDeployment <- function(appDir, name, account, bundleId, url) {
   invisible(NULL)
 }
 
+#' Read Application Deployments
+#' 
+#' Show deployment records for a given application directory. 
+#' @param appDir The directory from which to read deployment records. 
+#' @param nameFilter Return only deployments matching the given name (optional)
+#' @param accountFilter Return only deployments matching the given account
+#'   (optional)
+#' @examples
+#' \dontrun{
+#' 
+#' # Show all deployments of the ~/r/myapp directory made with the 'abc' account
+#' readDeployments("~/r/myapp", accountFilter="abc")
+#' }
+#' @seealso \code{\link{applications}} to get a list of deployments from the 
+#'   server, and \code{\link{deployApp}} to create a new deployment.
+#' @export
 readDeployments <- function(appDir, nameFilter = NULL, accountFilter = NULL) {
   
   deployments <- deploymentRecord(name = character(),
