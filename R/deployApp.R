185a5e41f6e042be077523c3a2b1498013b3c521
Andy Kipp <andy@rstudio.com>
2014-02-11 16:06:40 -0500

    Add application id to deploy status message

--- R/deployApp.R
+++ R/deployApp.R
@@ -99,7 +99,9 @@ deployApp <- function(appDir = getwd(),
   }
 
   # wait for the deployment to complete (will raise an error if it can't)
-  displayStatus("Deploying application...\n")
+  displayStatus(paste("Deploying application: ", 
+                      application$id, 
+                      "...\n", sep=""))
   task <- lucid$deployApplication(application$id, bundle$id)
   lucid$waitForTaskCompletion(task$task_id, quiet)
   displayStatus(paste("Application successfully deployed to ", 
