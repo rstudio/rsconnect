7f419b4f91feeae8ada3a5b78e1b799f3bfc0240
Andy Kipp <andy@rstudio.com>
2015-06-11 11:34:38 -0400

    Fix duplcated default locale

--- R/locale.R
+++ R/locale.R
@@ -21,37 +21,34 @@ detectLocale.Unix <- function () {
   unlist(strsplit(Sys.getlocale("LC_CTYPE"), ".", fixed=TRUE))[1]
 }
 
-detectLocale.Windows <- function (useCache = 
+detectLocale.Windows <- function (useCache =
                                   getOption('rsconnect.locale.cache', TRUE)) {
-  
+
   # default locale
   locale <- 'en_US'
-  
-  # default locale
-  locale <- 'en_US'
-  
+
   cacheFile <- localeCacheFile()
   if (file.exists(cacheFile) && useCache) {
-    
+
     # get chached
-    cache <- as.list(readDcf(cacheFile, all=TRUE))  
-    
+    cache <- as.list(readDcf(cacheFile, all=TRUE))
+
     locale <- unlist(cache$locale)
-    
+
   } else {
-    
+
     tryCatch({
-      
+
       # get system locale
       locale <- systemLocale()
-      
+
       # write the user info
       write.dcf(list(locale = locale),
                 cacheFile,
                 width = 100)
-      
+
     }, error=function(e) {
-      warning(paste0("Error detecting locale: ", e, 
+      warning(paste0("Error detecting locale: ", e,
                      " (Using default: ", locale, ")"), call.=FALSE)
     })
   }
@@ -69,7 +66,7 @@ systemLocale <- function() {
   info <- systemInfo()
   raw <- as.character(info[[20]])
   parts <- strsplit(unlist(strsplit(raw, ";",  fixed=TRUE)), "-", fixed=TRUE)
-  
+
   if (length(parts[[1]]) >= 2) {
     # normalize locale to something like en_US
     locale <- paste(tolower(parts[[1]][1]), toupper(parts[[1]][2]), sep="_")
