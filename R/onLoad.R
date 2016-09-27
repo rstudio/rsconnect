fc8174c2df49cf815e58a34cd2af83e4a7192249
Jonathan McPherson <jonathan@rstudio.com>
2016-09-27 12:53:04 -0700

    increase max bundle size, and add file count throttle (fixes #124)

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   if (is.null(getOption("rsconnect.max.bundle.size"))) {
-    options(rsconnect.max.bundle.size = 1048576000)
+    options(rsconnect.max.bundle.size = 3145728000)
+  }
+  if (is.null(getOption("rsconnect.max.bundle.files"))) {
+    options(rsconnect.max.bundle.files = 10000)
   }
 }
