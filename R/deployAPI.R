3bb04c66e587ec52519d08f840a84003d1db3c95
Scott Jackson <scottmmjackson@gmail.com>
2017-06-06 14:49:50 -0700

    Roll back plumber version to 0.3.2

--- R/deployAPI.R
+++ R/deployAPI.R
@@ -13,8 +13,8 @@
 deployAPI <- function(api,
                       ...) {
   if (!requireNamespace("plumber") ||
-      packageVersion("plumber") < "0.3.3") {
-    stop("Version 0.3.3 or later of the plumber package is required to ",
+      packageVersion("plumber") < "0.3.2") {
+    stop("Version 0.3.2 or later of the plumber package is required to ",
          "deploy plumber APIs.")
   }
   if (!file.exists(api)) {
