32aae37bbe02904fe0bfe1c33555bbc453b68a2f
Jonathan McPherson <jonathan@rstudio.com>
2021-04-01 14:35:43 -0700

    clean up configuration directory after tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,13 @@
 library(testthat)
 library(rsconnect)
 
+# record whether the configuration directory exists
+configDir <- rsconnectConfigDir()
+configDirExists <- file.exists(rsconnectConfigDir)
+
 test_check("rsconnect")
+
+# if the configuration directory did not exist before running the tests, clean it up.
+if (!configDirExists) {
+  unlink(configDir, recursive = TRUE)
+}
