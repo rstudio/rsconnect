a57d525bfc6532b7f654f5350343c995c97b19a6
Toph Allen <toph.allen@gmail.com>
2022-03-28 11:26:01 -0400

    Remove quarto-r dependency from tests

--- tests/testthat/test-bundle.R
+++ tests/testthat/test-bundle.R
@@ -26,10 +26,28 @@ fakeQuartoMetadata <- function(version, engines) {
   return(metadata)
 }
 
+quarto_path <- function() {
+  path_env <- Sys.getenv("QUARTO_PATH", unset = NA)
+  if (!is.na(path_env)) {
+    return(path_env)
+  } else {
+    locations <- c(
+      "quarto", # Use PATH
+      "/usr/local/bin/quarto", # Location used by some installers
+      "/opt/quarto/bin/quarto", # Location used by some installers
+      "/Applications/RStudio.app/Contents/MacOS/quarto/bin/quarto" # IDE embedded
+    )
+    for (location in locations) {
+      path <- unname(Sys.which(location))
+      if (nzchar(path)) return(path)
+    }
+    return(NULL)
+  }
+}
+
 quartoPathOrSkip <- function() {
   skip_on_cran()
-  skip_if_not_installed("quarto")
-  quarto <- quarto::quarto_path()
+  quarto <- quarto_path()
   skip_if(is.null(quarto), "quarto cli is not installed")
   return(quarto)
 }
