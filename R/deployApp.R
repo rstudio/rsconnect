6c323858533afe280011621b18e5b9c4f81abdce
Toph Allen <toph.allen@gmail.com>
2022-05-20 12:59:16 -0400

    more reorg

--- R/deployApp.R
+++ R/deployApp.R
@@ -506,96 +506,6 @@ deployApp <- function(appDir = getwd(),
   invisible(deploymentSucceeded)
 }
 
-
-# Does almost exactly the same work as writeManifest(), with slight
-# modifications to work within deployApp()
-bundleApp <- function(appName, appDir, appFiles, appPrimaryDoc, assetTypeName,
-                      contentCategory, verbose = FALSE, python = NULL,
-                      condaMode = FALSE, forceGenerate = FALSE, quarto = NULL,
-                      isShinyApps = FALSE, metadata = list(), image = NULL) {
-  logger <- verboseLogger(verbose)
-
-  quartoInfo <- inferQuartoInfo(
-    appDir = appDir,
-    appPrimaryDoc = appPrimaryDoc,
-    quarto = quarto,
-    metadata = metadata
-  )
-
-  logger("Inferring App mode and parameters")
-  appMode <- inferAppMode(
-    appDir = appDir,
-    appPrimaryDoc = appPrimaryDoc,
-    files = appFiles,
-    quartoInfo = quartoInfo
-  )
-  appPrimaryDoc <- inferAppPrimaryDoc(
-    appPrimaryDoc = appPrimaryDoc,
-    appFiles = appFiles,
-    appMode = appMode
-  )
-  hasParameters <- appHasParameters(
-    appDir = appDir,
-    appPrimaryDoc = appPrimaryDoc,
-    appMode = appMode,
-    contentCategory = contentCategory
-  )
-  documentsHavePython <- detectPythonInDocuments(
-    appDir = appDir,
-    files = appFiles
-  )
-
-  # get application users (for non-document deployments)
-  users <- NULL
-  if (is.null(appPrimaryDoc)) {
-    users <- suppressWarnings(authorizedUsers(appDir))
-  }
-
-  # copy files to bundle dir to stage
-  logger("Bundling app dir")
-  bundleDir <- bundleAppDir(
-    appDir = appDir,
-    appFiles = appFiles,
-    appPrimaryDoc = appPrimaryDoc
-  )
-  on.exit(unlink(bundleDir, recursive = TRUE), add = TRUE)
-
-  # generate the manifest and write it into the bundle dir
-  logger("Generate manifest.json")
-  manifest <- createAppManifest(
-    appDir = bundleDir,
-    appMode = appMode,
-    contentCategory = contentCategory,
-    hasParameters = hasParameters,
-    appPrimaryDoc = appPrimaryDoc,
-    assetTypeName = assetTypeName,
-    users = users,
-    condaMode = condaMode,
-    forceGenerate = forceGenerate,
-    python = python,
-    documentsHavePython = documentsHavePython,
-    retainPackratDirectory = TRUE,
-    quartoInfo = quartoInfo,
-    isShinyApps = isShinyApps,
-    image = image,
-    verbose = verbose
-  )
-  manifestJson <- enc2utf8(toJSON(manifest, pretty = TRUE))
-  manifestPath <- file.path(bundleDir, "manifest.json")
-  writeLines(manifestJson, manifestPath, useBytes = TRUE)
-
-  # if necessary write an index.htm for shinydoc deployments
-  logger("Writing Rmd index if necessary")
-  indexFiles <- writeRmdIndex(appName, bundleDir)
-
-  # create the bundle and return its path
-  logger("Compressing the bundle")
-  bundlePath <- tempfile("rsconnect-bundle", fileext = ".tar.gz")
-  writeBundle(bundleDir, bundlePath)
-  bundlePath
-}
-
-
 getPython <- function(path) {
   if (is.null(path)) {
     path <- Sys.getenv("RETICULATE_PYTHON",
@@ -826,7 +736,6 @@ getAppById <- function(id, account = NULL, server = NULL, hostUrl = NULL) {
 }
 
 applicationForTarget <- function(client, accountInfo, target, forceUpdate) {
-
   if (is.null(target$appId)) {
     # list the existing applications for this account and see if we
     # need to create a new application
@@ -861,7 +770,6 @@ validURL <- function(url) {
 }
 
 openURL <- function(client, application, launch.browser, on.failure, deploymentSucceeded) {
-
   # function to browse to a URL using user-supplied browser (config or final)
   showURL <- function(url) {
     if (isTRUE(launch.browser))
