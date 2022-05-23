e3ae443b1f9ef0e197afb5e38128ce922eb6c6f7
Toph Allen <toph.allen@gmail.com>
2022-05-23 17:02:50 -0400

    Move functions around

--- R/deploymentUtils.R
+++ R/deploymentUtils.R
@@ -1,4 +1,9 @@
-bundleAppDir <- function(appDir, appFiles, appPrimaryDoc = NULL, verbose = FALSE) {
+# Given a path to an directory and a list of files in that directory, copies
+ # those files to a new temporary directory. Performes some small modifications
+ # in this process, including renaming single-file Shiny apps to "app.R" and
+ # stripping packrat and renv commands from .Rprofile. Returns the path to the
+ # temporary directory.
+ bundleAppDir <- function(appDir, appFiles, appPrimaryDoc = NULL, verbose = FALSE) {
   if (verbose)
     timestampedLog("Creating tempfile for appdir")
   # create a directory to stage the application bundle in
@@ -219,85 +224,7 @@ enforceBundleLimits <- function(appDir, totalSize, totalFiles) {
   }
 }
 
-bundleApp <- function(appName, appDir, appFiles, appPrimaryDoc, assetTypeName,
-                      contentCategory, verbose = FALSE, python = NULL,
-                      condaMode = FALSE, forceGenerate = FALSE, quarto = NULL,
-                      isShinyApps = FALSE, metadata = list(), image = NULL) {
-  logger <- verboseLogger(verbose)
 
-  quartoInfo <- inferQuartoInfo(
-    appDir = appDir,
-    appPrimaryDoc = appPrimaryDoc,
-    quarto = quarto,
-    metadata = metadata
-  )
-
-  logger("Inferring App mode and parameters")
-  appMode <- inferAppMode(
-      appDir = appDir,
-      appPrimaryDoc = appPrimaryDoc,
-      files = appFiles,
-      quartoInfo = quartoInfo)
-  appPrimaryDoc <- inferAppPrimaryDoc(
-      appPrimaryDoc = appPrimaryDoc,
-      appFiles = appFiles,
-      appMode = appMode)
-  hasParameters <- appHasParameters(
-      appDir = appDir,
-      appPrimaryDoc = appPrimaryDoc,
-      appMode = appMode,
-      contentCategory = contentCategory)
-  documentsHavePython <- detectPythonInDocuments(
-      appDir = appDir,
-      files = appFiles)
-
-  # get application users (for non-document deployments)
-  users <- NULL
-  if (is.null(appPrimaryDoc)) {
-    users <- suppressWarnings(authorizedUsers(appDir))
-  }
-
-  # copy files to bundle dir to stage
-  logger("Bundling app dir")
-  bundleDir <- bundleAppDir(
-      appDir = appDir,
-      appFiles = appFiles,
-      appPrimaryDoc = appPrimaryDoc)
-  on.exit(unlink(bundleDir, recursive = TRUE), add = TRUE)
-
-  # generate the manifest and write it into the bundle dir
-  logger("Generate manifest.json")
-  manifest <- createAppManifest(
-      appDir = bundleDir,
-      appMode = appMode,
-      contentCategory = contentCategory,
-      hasParameters = hasParameters,
-      appPrimaryDoc = appPrimaryDoc,
-      assetTypeName = assetTypeName,
-      users = users,
-      condaMode = condaMode,
-      forceGenerate = forceGenerate,
-      python = python,
-      documentsHavePython = documentsHavePython,
-      retainPackratDirectory = TRUE,
-      quartoInfo = quartoInfo,
-      isShinyApps = isShinyApps,
-      image = image,
-      verbose = verbose)
-  manifestJson <- enc2utf8(toJSON(manifest, pretty = TRUE))
-  manifestPath <- file.path(bundleDir, "manifest.json")
-  writeLines(manifestJson, manifestPath, useBytes = TRUE)
-
-  # if necessary write an index.htm for shinydoc deployments
-  logger("Writing Rmd index if necessary")
-  indexFiles <- writeRmdIndex(appName, bundleDir)
-
-  # create the bundle and return its path
-  logger("Compressing the bundle")
-  bundlePath <- tempfile("rsconnect-bundle", fileext = ".tar.gz")
-  writeBundle(bundleDir, bundlePath)
-  bundlePath
-}
 
 # Writes a tar.gz file located at bundlePath containing all files in bundleDir.
 writeBundle <- function(bundleDir, bundlePath, verbose = FALSE) {
@@ -364,130 +291,6 @@ detectLongNames <- function(bundleDir, lengthLimit = 32) {
   return(invisible(FALSE))
 }
 
-
-#' Create a manifest.json describing deployment requirements.
-#'
-#' Given a directory content targeted for deployment, write a manifest.json into
-#' that directory describing the deployment requirements for that content.
-#'
-#' @param appDir Directory containing the content (Shiny application, R Markdown
-#'   document, etc).
-#'
-#' @param appFiles Optional. The full set of files and directories to be
-#'   included in future deployments of this content. Used when computing
-#'   dependency requirements. When `NULL`, all files in `appDir` are considered.
-#'
-#' @param appPrimaryDoc Optional. Specifies the primary document in a content
-#'   directory containing more than one. If `NULL`, the primary document is
-#'   inferred from the file list.
-#'
-#' @param contentCategory Optional. Specifies the kind of content being deployed
-#'   (e.g. `"plot"` or `"site"`).
-#'
-#' @param python Optional. Full path to a Python binary for use by `reticulate`.
-#'   The specified Python binary will be invoked to determine its version and to
-#'   list the Python packages installed in the environment. If `python = NULL`,
-#'   and `RETICULATE_PYTHON` is set in the environment, its value will be used.
-#'
-#' @param forceGeneratePythonEnvironment Optional. If an existing
-#'   `requirements.txt` file is found, it will be overwritten when this argument
-#'   is `TRUE`.
-#'
-#' @param quarto Optional. Full path to a Quarto binary for use deploying Quarto
-#'   content. The provided Quarto binary will be used to run `quarto inspect`
-#'   to gather information about the content.
-#'
-#' @param image Optional. The name of the image to use when building and
-#'   executing this content. If none is provided, RStudio Connect will
-#'   attempt to choose an image based on the content requirements.
-#'
-#' @param verbose If TRUE, prints progress messages to the console
-#'
-#'
-#' @export
-writeManifest <- function(appDir = getwd(),
-                          appFiles = NULL,
-                          appPrimaryDoc = NULL,
-                          contentCategory = NULL,
-                          python = NULL,
-                          forceGeneratePythonEnvironment = FALSE,
-                          quarto = NULL,
-                          image = NULL,
-                          verbose = FALSE) {
-
-  condaMode <- FALSE
-
-  if (is.null(appFiles)) {
-    appFiles <- bundleFiles(appDir)
-  } else {
-    appFiles <- explodeFiles(appDir, appFiles)
-  }
-
-  quartoInfo <- inferQuartoInfo(
-    appDir = appDir,
-    appPrimaryDoc = appPrimaryDoc,
-    quarto = quarto,
-    metadata = list()
-  )
-
-  appMode <- inferAppMode(
-      appDir = appDir,
-      appPrimaryDoc = appPrimaryDoc,
-      files = appFiles,
-      quartoInfo = quartoInfo)
-  appPrimaryDoc <- inferAppPrimaryDoc(
-      appPrimaryDoc = appPrimaryDoc,
-      appFiles = appFiles,
-      appMode = appMode)
-  hasParameters <- appHasParameters(
-      appDir = appDir,
-      appPrimaryDoc = appPrimaryDoc,
-      appMode = appMode,
-      contentCategory = contentCategory)
-  documentsHavePython <- detectPythonInDocuments(
-      appDir = appDir,
-      files = appFiles)
-
-  # copy files to bundle dir to stage
-  bundleDir <- bundleAppDir(
-      appDir = appDir,
-      appFiles = appFiles,
-      appPrimaryDoc = appPrimaryDoc)
-  on.exit(unlink(bundleDir, recursive = TRUE), add = TRUE)
-
-  python <- getPython(python)
-
-  # generate the manifest and write it into the bundle dir
-  manifest <- createAppManifest(
-      appDir = bundleDir,
-      appMode = appMode,
-      contentCategory = contentCategory,
-      hasParameters = hasParameters,
-      appPrimaryDoc = appPrimaryDoc,
-      assetTypeName = "content",
-      users = NULL,
-      condaMode = condaMode,
-      forceGenerate = forceGeneratePythonEnvironment,
-      python = python,
-      documentsHavePython = documentsHavePython,
-      retainPackratDirectory = FALSE,
-      quartoInfo = quartoInfo,
-      isShinyApps = FALSE,
-      image = image,
-      verbose = verbose)
-  manifestJson <- enc2utf8(toJSON(manifest, pretty = TRUE))
-  manifestPath <- file.path(appDir, "manifest.json")
-  writeLines(manifestJson, manifestPath, useBytes = TRUE)
-
-  requirementsFilename <- manifest$python$package_manager$package_file
-  if (is.null(requirementsFilename)) { requirementsFilename <- "requirements.txt" }
-  srcRequirementsFile <- file.path(bundleDir, requirementsFilename)
-  dstRequirementsFile <- file.path(appDir, requirementsFilename)
-  if(file.exists(srcRequirementsFile) && !file.exists(dstRequirementsFile)) {
-    file.copy(srcRequirementsFile, dstRequirementsFile)
-  }
-  invisible()
-}
 
 yamlFromRmd <- function(filename) {
   lines <- readLines(filename, warn = FALSE, encoding = "UTF-8")
@@ -736,6 +539,17 @@ getCondaExeForPrefix <- function(prefix) {
     stop(paste("Conda env prefix", prefix, "does not have the `conda` command line interface."))
   }
   conda
+}
+
+getPython <- function(path) {
+  if (is.null(path)) {
+    path <- Sys.getenv("RETICULATE_PYTHON",
+                       unset = Sys.getenv("RETICULATE_PYTHON_FALLBACK"))
+    if (path == "") {
+      return(NULL)
+    }
+  }
+  path.expand(path)
 }
 
 inferPythonEnv <- function(workdir, python, condaMode, forceGenerate) {
