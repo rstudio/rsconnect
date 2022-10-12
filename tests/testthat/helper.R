c4ee2f15d1676ef46e9abe5db584e9118c34f1e3
Aron Atkins <aron@gweep.net>
2022-10-12 16:46:05 -0400

    shift helper to helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -21,3 +21,41 @@ httpTestRecorder <- function(protocol, host, port, method, path, headers,
     timeout = timeout
   )
 }
+
+# Create directories as temporary replacements for HOME and R_USER_CONFIG_DIR
+ # to avoid having tests overwrite the "official" configuration locations.
+ #
+ # test_that("some test", {
+ #   havingFakeConfig({
+ #     take_some_action(...)
+ #     expect_something(...)
+ #   })
+ # })
+havingFakeConfig <- function(expr) {
+  temp_home_dir <- tempfile("home-")
+  dir.create(temp_home_dir)
+  temp_config_dir <- tempfile("config-")
+  dir.create(temp_config_dir)
+  
+  home <- Sys.getenv("HOME")
+  config <- Sys.getenv("R_USER_CONFIG_DIR")
+
+  on.exit({
+    # clean up temp folders
+    unlink(temp_home_dir, recursive = TRUE)
+    unlink(temp_config_dir, recursive = TRUE)
+
+    # restore environment
+    Sys.setenv(HOME = home)
+    Sys.setenv(R_USER_CONFIG_DIR = config)
+  }, add = TRUE)
+
+  # temporarily change home to a temp folder so we don't litter the actual
+  # home / config folder with test output
+  Sys.setenv(HOME = temp_home_dir)
+  # temporarily change the user config to use a temp folder so we don't litter
+  # the actual config folder with test output
+  Sys.setenv(R_USER_CONFIG_DIR = temp_config_dir)
+
+  eval(expr)
+}
--- tests/testthat/test-cert.R
+++ tests/testthat/test-cert.R
@@ -1,34 +1,5 @@
 context("certificates")
 
-havingFakeConfig <- function(expr) {
-  temp_home_dir <- tempfile("home-")
-  dir.create(temp_home_dir)
-  temp_config_dir <- tempfile("config-")
-  dir.create(temp_config_dir)
-  
-  home <- Sys.getenv("HOME")
-  config <- Sys.getenv("R_USER_CONFIG_DIR")
-
-  on.exit({
-    # clean up temp folders
-    unlink(temp_home_dir, recursive = TRUE)
-    unlink(temp_config_dir, recursive = TRUE)
-
-    # restore environment
-    Sys.setenv(HOME = home)
-    Sys.setenv(R_USER_CONFIG_DIR = config)
-  }, add = TRUE)
-
-  # temporarily change home to a temp folder so we don't litter the actual
-  # home / config folder with test output
-  Sys.setenv(HOME = temp_home_dir)
-  # temporarily change the user config to use a temp folder so we don't litter
-  # the actual config folder with test output
-  Sys.setenv(R_USER_CONFIG_DIR = temp_config_dir)
-
-  eval(expr)
-}
-
 havingHttpRecorder <- function(expr) {
   http <- getOption("rsconnect.http")
 
