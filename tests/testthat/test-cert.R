0456499e623574a590aa8a97b09f928cee65a7b3
Aron Atkins <aron@gweep.net>
2022-10-12 16:40:34 -0400

    swap config and home

--- tests/testthat/test-cert.R
+++ tests/testthat/test-cert.R
@@ -3,20 +3,28 @@ context("certificates")
 havingFakeConfig <- function(expr) {
   temp_home_dir <- tempfile("home-")
   dir.create(temp_home_dir)
-
+  temp_config_dir <- tempfile("config-")
+  dir.create(temp_config_dir)
+  
   home <- Sys.getenv("HOME")
+  config <- Sys.getenv("R_USER_CONFIG_DIR")
 
   on.exit({
-    # clean up temp folder
+    # clean up temp folders
     unlink(temp_home_dir, recursive = TRUE)
+    unlink(temp_config_dir, recursive = TRUE)
 
-    # restore HOME
+    # restore environment
     Sys.setenv(HOME = home)
+    Sys.setenv(R_USER_CONFIG_DIR = config)
   }, add = TRUE)
 
   # temporarily change home to a temp folder so we don't litter the actual
-  # config folder with test output
+  # home / config folder with test output
   Sys.setenv(HOME = temp_home_dir)
+  # temporarily change the user config to use a temp folder so we don't litter
+  # the actual config folder with test output
+  Sys.setenv(R_USER_CONFIG_DIR = temp_config_dir)
 
   eval(expr)
 }
