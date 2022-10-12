a84a174f8b21f86d01bbe380851a93941517c713
Aron Atkins <aron@gweep.net>
2022-10-12 16:42:28 -0400

    invert wrapping

--- tests/testthat/test-cert.R
+++ tests/testthat/test-cert.R
@@ -43,102 +43,120 @@ havingHttpRecorder <- function(expr) {
   eval(expr)
 }
 
-cert_test_that <- function(test, expr) {
+test_that("certificates can be saved", {
   havingFakeConfig(
-    havingHttpRecorder(
-      test_that(test, expr)
-    )
+    havingHttpRecorder({
+
+      # add a server with a sample certificate
+      addServer(url = "https://localhost:4567/",
+                name = "cert_test_a",
+                certificate = "certs/sample.crt",
+                quiet = FALSE)
+
+      # read it back
+      info <- serverInfo("cert_test_a")
+
+      # compare with the contents of the cert we read
+      certLines <- paste(readLines("certs/sample.crt"), collapse = "\n")
+      expect_equal(certLines, info$certificate)
+    })
   )
-}
-
-cert_test_that("certificates can be saved", {
-
-  # add a server with a sample certificate
-  addServer(url = "https://localhost:4567/",
-            name = "cert_test_a",
-            certificate = "certs/sample.crt",
-            quiet = FALSE)
-
-  # read it back
-  info <- serverInfo("cert_test_a")
-
-  # compare with the contents of the cert we read
-  certLines <- paste(readLines("certs/sample.crt"), collapse = "\n")
-  expect_equal(certLines, info$certificate)
 })
 
-cert_test_that("certificates can be added", {
-  # add a server without a certificate
-  addServer(url = "https://localhost:4567/",
-            name = "cert_test_b",
-            quiet = FALSE)
+test_that("certificates can be added", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      # add a server without a certificate
+      addServer(url = "https://localhost:4567/",
+                name = "cert_test_b",
+                quiet = FALSE)
 
-  # add the certificate
-  addServerCertificate(name = "cert_test_b",
-                       certificate = "certs/sample.crt")
+      # add the certificate
+      addServerCertificate(name = "cert_test_b",
+                           certificate = "certs/sample.crt")
 
-  # read the server info
-  info <- serverInfo("cert_test_b")
+      # read the server info
+      info <- serverInfo("cert_test_b")
 
-  # see if the cert we added is there
-  certLines <- paste(readLines("certs/sample.crt"), collapse = "\n")
-  expect_equal(certLines, info$certificate)
+      # see if the cert we added is there
+      certLines <- paste(readLines("certs/sample.crt"), collapse = "\n")
+      expect_equal(certLines, info$certificate)
+    })
+  )
 })
 
-cert_test_that("certificates can't be attached to plain http servers", {
-  expect_error(addServer(url = "http://localhost:4567",
-                         name = "cert_test_c",
-                         certificate = "certs/sample.crt"))
-  addServer(url = "http://localhost:4567", name = "cert_test_d")
-  expect_error(addServerCertificate(name = "cert_test_d",
-                                    certificate = "certs/sample.crt"))
+test_that("certificates can't be attached to plain http servers", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      expect_error(addServer(url = "http://localhost:4567",
+                             name = "cert_test_c",
+                             certificate = "certs/sample.crt"))
+      addServer(url = "http://localhost:4567", name = "cert_test_d")
+      expect_error(addServerCertificate(name = "cert_test_d",
+                                        certificate = "certs/sample.crt"))
+    })
+  )
 })
 
-cert_test_that("system and server cert stores are concatenated", {
-  # use a dummy CA bundle
-  oldCaOpt <- getOption("rsconnect.ca.bundle")
-  options(rsconnect.ca.bundle = file.path(getwd(), "certs", "store.crt"))
-  on.exit(expr = {
-    options(rsconnect.ca.bundle = oldCaOpt)
-  }, add = TRUE)
+test_that("system and server cert stores are concatenated", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      # use a dummy CA bundle
+      oldCaOpt <- getOption("rsconnect.ca.bundle")
+      options(rsconnect.ca.bundle = file.path(getwd(), "certs", "store.crt"))
+      on.exit(expr = {
+        options(rsconnect.ca.bundle = oldCaOpt)
+      }, add = TRUE)
 
-  # create and then read the temporary certificate file
-  concatenated <- createCertificateFile(readLines("certs/localhost.crt"))
-  on.exit(expr = {
-    unlink(concatenated)
-  }, add = TRUE)
-  store <- paste(readLines(concatenated), collapse = "\n")
+      # create and then read the temporary certificate file
+      concatenated <- createCertificateFile(readLines("certs/localhost.crt"))
+      on.exit(expr = {
+        unlink(concatenated)
+      }, add = TRUE)
+      store <- paste(readLines(concatenated), collapse = "\n")
 
-  # make sure that the localhost and system stores both exist in the
-  # concatenated store
-  expect_true(grepl("localhost", store, fixed = TRUE))
-  expect_true(grepl("system", store, fixed = TRUE))
+      # make sure that the localhost and system stores both exist in the
+      # concatenated store
+      expect_true(grepl("localhost", store, fixed = TRUE))
+      expect_true(grepl("system", store, fixed = TRUE))
+    })
+  )
 })
 
-cert_test_that("invalid certificates cannot be added", {
-  expect_error(
-    addServer(url = "https://localhost:4567/",
-              name = "cert_test_e",
-              certificate = "certs/invalid.crt",
-              quiet = FALSE))
+test_that("invalid certificates cannot be added", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      expect_error(
+        addServer(url = "https://localhost:4567/",
+                  name = "cert_test_e",
+                  certificate = "certs/invalid.crt",
+                  quiet = FALSE))
+    })
+  )
 })
 
-cert_test_that("multiple certificates can exist in the same file", {
-  addServer(url = "https://localhost:4567/",
-            name = "cert_test_f",
-            certificate = "certs/two-cas.crt",
-            quiet = FALSE)
+test_that("multiple certificates can exist in the same file", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      addServer(url = "https://localhost:4567/",
+                name = "cert_test_f",
+                certificate = "certs/two-cas.crt",
+                quiet = FALSE)
 
-  # read it back
-  info <- serverInfo("cert_test_f")
+      # read it back
+      info <- serverInfo("cert_test_f")
 
-  # compare with the contents of the cert we read
-  certLines <- paste(readLines("certs/two-cas.crt"), collapse = "\n")
-  expect_equal(certLines, info$certificate)
+      # compare with the contents of the cert we read
+      certLines <- paste(readLines("certs/two-cas.crt"), collapse = "\n")
+      expect_equal(certLines, info$certificate)
+    })
+  )
 })
 
-cert_test_that("certificates not used when making plain http connections", {
-  GET(list(
+test_that("certificates not used when making plain http connections", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      GET(list(
         protocol  = "http",
         host      = "localhost:4567",
         port      = "80",
@@ -148,11 +166,15 @@ cert_test_that("certificates not used when making plain http connections", {
         certificate = "certs/localhost.crt"
       ),
       "apps")
-  expect_equal(httpLastRequest$certificate, NULL)
+      expect_equal(httpLastRequest$certificate, NULL)
+    })
+  )
 })
 
-cert_test_that("certificates used when making https connections", {
-  GET(list(
+test_that("certificates used when making https connections", {
+  havingFakeConfig(
+    havingHttpRecorder({
+      GET(list(
         protocol  = "https",
         host      = "localhost:4567",
         port      = "443",
@@ -163,9 +185,11 @@ cert_test_that("certificates used when making https connections", {
       ),
       "apps")
 
-  # we expect to get a cert file
-  expect_true(file.exists(httpLastRequest$certificate))
+      # we expect to get a cert file
+      expect_true(file.exists(httpLastRequest$certificate))
 
-  # clean up
-  unlink(httpLastRequest$certificate)
+      # clean up
+      unlink(httpLastRequest$certificate)
+    })
+  )
 })
