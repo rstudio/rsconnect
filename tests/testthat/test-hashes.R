dffcb6451e56a6371d4808b3c35403ff0b02b53c
Hadley Wickham <h.wickham@gmail.com>
2023-02-09 08:42:54 -0600

    Make hash tests self-contained

--- tests/testthat/test-hashes.R
+++ tests/testthat/test-hashes.R
@@ -1,43 +1,14 @@
-emptyFile <- tempfile(fileext = ".empty.txt")
-contentsFile <- tempfile(fileext = ".contents.txt")
-
-# computed by openssl::md5("")
-expectedEmptyMD5 <- "d41d8cd98f00b204e9800998ecf8427e"
-# computed by openssl::md5("go bananas!\n")
-expectedContentsMD5 <- "52d2daa95d288f3c01e4d4d87f85727e"
-# computed by openssl::md5("")
-expectedMissingMD5 <- "d41d8cd98f00b204e9800998ecf8427e"
-
-setup({
-  # Create an empty file.
-  if (!dir.exists(dirname(emptyFile))) {
-    dir.create(dirname(emptyFile), recursive = TRUE)
-  }
-  file.create(emptyFile)
-
-  # Create a file with well-known contents.
-  if (!dir.exists(dirname(contentsFile))) {
-    dir.create(dirname(contentsFile), recursive = TRUE)
-  }
-
-  # Open in binary mode so the contents are identical on all platforms
-  # (otherwise the file contains a \n on Unix and \r\n on Windows, which
-  # hash differently)
-  con <- file(contentsFile, open = "wb")
-  writeLines("go bananas!", con)
-  close(con)
-})
-
-teardown({
-  unlink(emptyFile)
-  unlink(contentsFile)
-})
-
 expect_hash_type <- function(object) {
   expect_type(object, "raw")
 }
 
 test_that("we can hash an empty file", {
+  emptyFile <- withr::local_tempfile()
+  file.create(emptyFile)
+
+  # computed by openssl::md5("")
+  expectedEmptyMD5 <- "d41d8cd98f00b204e9800998ecf8427e"
+
   rawEmptyMD5 <- fileMD5(emptyFile)
   expect_hash_type(rawEmptyMD5)
   emptyMD5 <- md5.as.string(rawEmptyMD5)
@@ -50,6 +21,18 @@ test_that("we can hash an empty file", {
 })
 
 test_that("we can hash a file with well known contents", {
+  contentsFile <- withr::local_tempfile()
+  # Open in binary mode so the contents are identical on all platforms
+  # (otherwise the file contains a \n on Unix and \r\n on Windows, which
+  # hash differently)
+  con <- file(contentsFile, open = "wb")
+  writeLines("go bananas!", con)
+
+  # computed by openssl::md5("go bananas!\n")
+  expectedContentsMD5 <- "52d2daa95d288f3c01e4d4d87f85727e"
+
+  close(con)
+
   rawContentsMD5 <- fileMD5(contentsFile)
   expect_hash_type(rawContentsMD5)
   contentsMD5 <- md5.as.string(rawContentsMD5)
@@ -62,6 +45,9 @@ test_that("we can hash a file with well known contents", {
 })
 
 test_that("we can hash when not supplied a filename", {
+  # computed by openssl::md5("")
+  expectedMissingMD5 <- "d41d8cd98f00b204e9800998ecf8427e"
+
   rawMissingMD5 <- fileMD5(NULL)
   expect_hash_type(rawMissingMD5)
   missingMD5 <- md5.as.string(rawMissingMD5)
