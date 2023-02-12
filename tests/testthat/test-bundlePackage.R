3a48d894ddceb23b71606e6e7019677e838da40c
Hadley Wickham <h.wickham@gmail.com>
2023-02-12 16:34:00 -0600

    Add some more basic tests

--- tests/testthat/test-bundlePackage.R
+++ tests/testthat/test-bundlePackage.R
@@ -1,3 +1,20 @@
+test_that("non-R apps don't have packages", {
+  app_dir <- withr::local_tempdir()
+  out <- bundlePackages(app_dir, appMode = "static")
+  expect_equal(out, list())
+
+  out <- bundlePackages(app_dir, appMode = "", quartoInfo = list(engines = "blather"))
+  expect_equal(out, list())
+})
+
+test_that("includes inferred dependencies", {
+  app_dir <- withr::local_tempdir()
+  out <- bundlePackages(app_dir, appMode = "rmd-static", hasParameters = TRUE)
+
+  expect_true("rmarkdown" %in% names(out))
+  expect_true("shiny" %in% names(out))
+})
+
 test_that("errors if dependencies aren't installed", {
   app_dir <- withr::local_tempdir()
   writeLines(con = file.path(app_dir, "index.Rmd"), c(
