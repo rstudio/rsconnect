3bf50f095e80fc58ea3b966cd40d925e111fdbbc
Hadley Wickham <hadley@posit.co>
2023-02-14 10:57:05 -0600

    Prepare `bundlePackages()` for refactoring (#664)
    * Extract `bundlePackage()` into own function/file and move helpers to same file
    * Ensure file completely covered by tests

---
+++ tests/testthat/_snaps/bundlePackage.md
@@ -0,0 +1,61 @@
+# errors if dependencies aren't installed
+
+    Code
+      bundlePackages(app_dir, appMode = "rmd-static", assetTypeName = "asset")
+    Condition
+      Error:
+      ! * Asset depends on package "doesntexist1"but it is not installed.
+        Please resolve before continuing.
+      
+      * Asset depends on package "doesntexist2"but it is not installed.
+        Please resolve before continuing.
+
+# warns if can't find source
+
+    Code
+      . <- bundlePackages(app_dir, appMode = "rmd-static", assetTypeName = "asset")
+    Condition
+      Warning:
+      * May be unable to deploy package dependency 'shiny'; could not
+        determine a repository URL for the source 'foo'.
+      
+      * Unable to determine the source location for some packages. Packages
+        should be installed from a package repository like CRAN or a version
+        control system. Check that options('repos') refers to a package
+        repository containing the needed package versions.
+
+# infers correct packages for each source
+
+    Code
+      inferRPackageDependencies("rmd-static")
+    Output
+      [1] "rmarkdown"
+    Code
+      inferRPackageDependencies("rmd-static", TRUE)
+    Output
+      [1] "shiny"     "rmarkdown"
+    Code
+      inferRPackageDependencies("quarto-static")
+    Output
+      [1] "rmarkdown"
+    Code
+      inferRPackageDependencies("quarto-shiny")
+    Output
+      [1] "rmarkdown" "shiny"    
+    Code
+      inferRPackageDependencies("rmd-shiny")
+    Output
+      [1] "rmarkdown" "shiny"    
+    Code
+      inferRPackageDependencies("shiny")
+    Output
+      [1] "shiny"
+    Code
+      inferRPackageDependencies("api")
+    Output
+      [1] "plumber"
+    Code
+      inferRPackageDependencies("api", documentsHavePython = TRUE)
+    Output
+      [1] "plumber"    "reticulate"
+
---
+++ tests/testthat/test-bundlePackage.R
@@ -0,0 +1,84 @@
+test_that("non-R apps don't have packages", {
+  app_dir <- withr::local_tempdir()
+  out <- bundlePackages(app_dir, appMode = "static")
+  expect_equal(out, list())
+
+  out <- bundlePackages(app_dir, appMode = "", quartoInfo = list(engines = "blather"))
+  expect_equal(out, list())
+})
+
+test_that("returns list of package details", {
+  app_dir <- withr::local_tempdir()
+  out <- bundlePackages(app_dir, appMode = "api", hasParameters = TRUE)
+
+  expect_type(out, "list")
+
+  common <- c("Source", "Repository", "description")
+  expect_equal(setdiff(common, names(out[[1]])), character())
+})
+
+test_that("includes inferred dependencies", {
+  app_dir <- withr::local_tempdir()
+  out <- bundlePackages(app_dir, appMode = "rmd-static", hasParameters = TRUE)
+
+  expect_true("rmarkdown" %in% names(out))
+  expect_true("shiny" %in% names(out))
+})
+
+test_that("errors if dependencies aren't installed", {
+  mockr::local_mock(snapshotRDependencies = function(...) {
+    data.frame(
+      Package = c("doesntexist1", "doesntexist2"),
+      Source = "CRAN",
+      Repository = "https://cran.rstudio.com"
+    )
+  })
+
+  app_dir <- withr::local_tempdir()
+  writeLines(con = file.path(app_dir, "index.Rmd"), c(
+    "```{r}",
+    "library(doesntexist1)",
+    "library(doesntexist2)",
+    "```"
+  ))
+
+  expect_snapshot(
+    bundlePackages(app_dir, appMode = "rmd-static", assetTypeName = "asset"),
+    error = TRUE
+  )
+})
+
+test_that("warns if can't find source", {
+  mockr::local_mock(snapshotRDependencies = function(...) {
+    data.frame(
+      Package = "shiny",
+      Source = "foo",
+      Repository = NA
+    )
+  })
+
+  app_dir <- withr::local_tempdir()
+  writeLines(con = file.path(app_dir, "index.Rmd"), c(
+    "```{r}",
+    "library(shiny)",
+    "```"
+  ))
+
+  expect_snapshot(
+    . <- bundlePackages(app_dir, appMode = "rmd-static", assetTypeName = "asset")
+  )
+})
+
+test_that("infers correct packages for each source", {
+  # Simple regression test in preparation for refactoring
+  expect_snapshot({
+    inferRPackageDependencies("rmd-static")
+    inferRPackageDependencies("rmd-static", TRUE)
+    inferRPackageDependencies("quarto-static")
+    inferRPackageDependencies("quarto-shiny")
+    inferRPackageDependencies("rmd-shiny")
+    inferRPackageDependencies("shiny")
+    inferRPackageDependencies("api")
+    inferRPackageDependencies("api", documentsHavePython = TRUE)
+  })
+})
