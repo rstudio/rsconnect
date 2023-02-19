b26cd780669d4098f35940d248bc3e779c849db7
Hadley Wickham <h.wickham@gmail.com>
2023-02-19 17:19:50 -0600

    Move tests across
    And make standalone

--- tests/testthat/test-accounts.R
+++ tests/testthat/test-accounts.R
@@ -1,33 +1,3 @@
-setupFakeAccounts <- function() {
-  registerUserApiKey("simple", "alice", 13, "alice-api-key")
-  registerUserApiKey("complex", "hatter+mad@example.com", 42, "hatter-api-key")
-}
-
-test_that("account file returned with server name", {
-  local_temp_config()
-  setupFakeAccounts()
-
-  expected <- normalizePath(file.path(
-    rsconnectConfigDir("accounts"),
-    "simple/alice.dcf"
-  ))
-  dir <- accountConfigFile("alice", server = "simple")
-  expect_equal(dir, expected)
-})
-
-test_that("account file containing pattern characters found with server name", {
-  local_temp_config()
-  setupFakeAccounts()
-
-  # https://github.com/rstudio/rsconnect/issues/620
-  expected <- normalizePath(file.path(
-    rsconnectConfigDir("accounts"),
-    "complex/hatter+mad@example.com.dcf"
-  ))
-  dir <- accountConfigFile("hatter+mad@example.com", server = "complex")
-  expect_equal(dir, expected)
-})
-
 test_that("All hosted product names are identified as cloud", {
   expect_true(isCloudServer("shinyapps.io"))
   expect_true(isCloudServer("rstudio.cloud"))
