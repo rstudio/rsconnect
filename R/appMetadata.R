39bb7f13fad85ea3752ce9101eee3bcead518d94
Hadley Wickham <h.wickham@gmail.com>
2023-02-20 07:40:13 -0600

    Move quarto_path() to helper

--- R/appMetadata.R
+++ R/appMetadata.R
@@ -325,22 +325,3 @@ quartoInspect <- function(quarto, appDir = NULL, appPrimaryDoc = NULL) {
   }
   return(NULL)
 }
-
-quarto_path <- function() {
-  path_env <- Sys.getenv("QUARTO_PATH", unset = NA)
-  if (!is.na(path_env)) {
-    return(path_env)
-  } else {
-    locations <- c(
-      "quarto", # Use PATH
-      "/usr/local/bin/quarto", # Location used by some installers
-      "/opt/quarto/bin/quarto", # Location used by some installers
-      "/Applications/RStudio.app/Contents/MacOS/quarto/bin/quarto" # macOS IDE
-    )
-    for (location in locations) {
-      path <- unname(Sys.which(location))
-      if (nzchar(path)) return(path)
-    }
-    return(NULL)
-  }
-}
