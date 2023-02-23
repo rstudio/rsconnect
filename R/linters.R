a95d46d756bd1bbaf2897b4dca6fe8d0cca365b2
Hadley Wickham <hadley@posit.co>
2023-02-23 07:41:29 -0600

    Lint refactoring 2 (#695)
    * Improve lint printing: don't return invisibly, add print method, use `cat()` instead of `message()`,print something if no problems
    * Move `lint()` to new file & re-organise tests
    * `standardizeAppFiles()` always returns relative directories so reduce path tweaking
    * Move `lint-utils.R` functions closer to usage
    * Use `sQuote`, not `shQuote`, for cross-platform reproducibility.

--- R/linters.R
+++ R/linters.R
@@ -1,3 +1,69 @@
+.__LINTERS__. <- new.env(parent = emptyenv())
+
+##' Add a Linter
+##'
+##' Add a linter, to be used in subsequent calls to [lint()].
+##'
+##' @param name The name of the linter, as a string.
+##' @param linter A [linter()].
+##' @export
+##' @example examples/example-linter.R
+addLinter <- function(name, linter) {
+  assign(name, linter, envir = .__LINTERS__.)
+}
+
+##' Create a Linter
+##'
+##' Generate a linter, which can identify errors or problematic regions in a
+##' project.
+##'
+##' @param apply Function that, given the content of a file, returns the indices
+##'   at which problems were found.
+##' @param takes Function that, given a set of paths, returns the subset of
+##'   paths that this linter uses.
+##' @param message Function that, given content and lines, returns an
+##'   informative message for the user. Typically generated with
+##'   [makeLinterMessage()].
+##' @param suggestion String giving a prescribed fix for the linted problem.
+##' @export
+##' @example examples/example-linter.R
+linter <- function(apply, takes, message, suggestion) {
+  result <- list(
+    apply = apply,
+    takes = takes,
+    message = message,
+    suggestion = suggestion
+  )
+  class(result) <- "linter"
+  result
+}
+
+getLinterApplicableFiles <- function(linter, files) {
+  result <- linter$takes(files)
+  if (is.numeric(result) || is.logical(result)) {
+    files[result]
+  } else {
+    result
+  }
+}
+
+applyLinter <- function(linter, ...) {
+  result <- linter$apply(...)
+  if (is.logical(result)) {
+    output <- which(result)
+  } else {
+    output <- as.numeric(result)
+  }
+  attributes(output) <- attributes(result)
+  output
+}
+
+
+isRCodeFile <- function(path) {
+  grepl("\\.[rR]$|\\.[rR]md$|\\.[rR]nw$", path)
+}
+
+
 addLinter("absolute.paths", linter(
 
   apply = function(content, ...) {
@@ -128,9 +194,9 @@ addLinter("filepath.capitalization", linter(
         if (length(from[[regex]][[i]]))
           paste(collapse = ", ",
                 paste("[",
-                      shQuote(from[[regex]][[i]]),
+                      sQuote(from[[regex]][[i]]),
                       " -> ",
-                      shQuote(to[[regex]][[i]]),
+                      sQuote(to[[regex]][[i]]),
                       "]", sep = "")
           )
         else
@@ -196,3 +262,107 @@ addLinter("browseURL", linter(
 
   suggestion = "Remove browseURL calls; browseURL does not work in deployed applications."
 ))
+
+
+stripComments <- function(content) {
+  gsub("#.*", "", content, perl = TRUE)
+}
+
+hasBrowserCalls <- function(content) {
+  # look for calls to browser(); they will cause a debug halt, which is almost
+  # never wanted in a production application
+  grepl("\\bbrowser\\([^)]*\\)", content, perl = TRUE)
+}
+
+hasBrowseURLCalls <- function(content) {
+  # look for calls to browseURL(); browsers can't be opened on the server in
+  # deployed applications
+  grepl("\\bbrowseURL\\([^)]*\\)", content, perl = TRUE)
+}
+
+hasAbsolutePaths <- function(content) {
+
+  regex <- c(
+    "\\[(.*?)\\]\\(\\s*[a-zA-Z]:/[^\"\']", ## windows-style markdown references [Some image](C:/...)
+    "\\[(.*?)\\]\\(\\s*/[^/]", ## unix-style markdown references [Some image](/Users/...)
+    NULL ## so we don't worry about commas above
+  )
+
+  regexResults <- as.logical(Reduce(`+`, lapply(regex, function(rex) {
+    grepl(rex, content, perl = TRUE)
+  })))
+
+  # Strip out all strings in the document, and check to see if any of them
+  # resolve to absolute paths on the system.
+  sQuoteRegex <- "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"
+  dQuoteRegex <- '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'
+
+  extractedStrings <- lapply(c(sQuoteRegex, dQuoteRegex), function(regex) {
+    matches <- gregexpr(regex, content, perl = TRUE)
+    lapply(seq_along(matches), function(i) {
+      match <- matches[[i]]
+      if (c(match[[1]]) == -1L) return(character())
+      starts <- as.integer(match) + 1
+      ends <- starts + attr(match, "match.length") - 3
+      substring(content[[i]], starts, ends)
+    })
+  })
+
+  strings <- vector("list", length(extractedStrings[[1]]))
+  for (i in seq_along(extractedStrings[[1]])) {
+    strings[[i]] <- unique(c(extractedStrings[[1]][[i]], extractedStrings[[2]][[i]]))
+    strings[[i]] <- strings[[i]][nchar(strings[[i]]) >= 5]
+  }
+
+  lineHasAbsolutePath <- unlist(lapply(strings, function(x) {
+    any(
+      grepl("^/|^[a-zA-Z]:/|^~", x, perl = TRUE) &
+      file.exists(x) &
+      !dirExists(x) &
+      vapply(gregexpr("[~/]", x, perl = TRUE), USE.NAMES = FALSE, FUN.VALUE = numeric(1), length) >= 3
+    )
+  }))
+
+  as.logical(lineHasAbsolutePath + regexResults)
+
+}
+
+noMatch <- function(x) {
+  identical(attr(x, "match.length"), -1L)
+}
+
+badRelativePaths <- function(content, project, path, ...) {
+
+  ## Figure out how deeply the path of the file is nested
+  ## (it is relative to the project root)
+  slashMatches <- gregexpr("/", path)
+  nestLevel <- if (noMatch(slashMatches)) 0 else length(slashMatches[[1]])
+
+  ## Identify occurrences of "../"
+  regexResults <- gregexpr("../", content, fixed = TRUE)
+
+  ## Figure out sequential runs of `../`
+  runs <- lapply(regexResults, function(x) {
+    if (noMatch(x)) return(NULL)
+    rle <- rle(as.integer(x) - seq(0, by = 3, length.out = length(x)))
+    rle$lengths
+  })
+
+  badPaths <- vapply(runs, function(x) {
+    any(x > nestLevel)
+  }, logical(1))
+
+  badPaths
+}
+
+
+transposeList <- function(list) {
+  unname(as.list(
+    as.data.frame(
+      t(
+        as.matrix(
+          as.data.frame(list, stringsAsFactors = FALSE)
+        )
+      ), stringsAsFactors = FALSE)
+  ))
+}
--- tests/testthat/_snaps/lint.md
+++ tests/testthat/_snaps/lint.md
@@ -1,3 +1,20 @@
+# lints give have useful print method
+
+    Code
+      lint(test_path("test-rmd-bad-case"))
+    Output
+      ---------
+      index.Rmd
+      ---------
+      The following lines contain paths to files not matching in case sensitivity:
+      29: ![](rstudio.svg)    ['rstudio.svg' -> 'RStudio.svg']
+      
+      Filepaths are case-sensitive on deployment server.
+    Code
+      lint(test_path("shinyapp-appR"))
+    Output
+      No problems found
+
 # checkLayout() errors if primary doc & app.R
 
     Code
