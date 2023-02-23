fc7eb289a0566182d7aa6d69b839b8d7f97d888c
Hadley Wickham <h.wickham@gmail.com>
2023-02-23 07:19:27 -0600

    Rename argument in deploymentTarget()

--- R/deploymentTarget.R
+++ R/deploymentTarget.R
@@ -1,6 +1,6 @@
 # calculate the deployment target based on the passed parameters and
 # any saved deployments that we have
-deploymentTarget <- function(appPath = ".",
+deploymentTarget <- function(recordPath = ".",
                              appName = NULL,
                              appTitle = NULL,
                              appId = NULL,
@@ -9,7 +9,7 @@ deploymentTarget <- function(appPath = ".",
                              error_call = caller_env()) {
 
   appDeployments <- deployments(
-    appPath = appPath,
+    appPath = recordPath,
     nameFilter = appName,
     accountFilter = account,
     serverFilter = server
@@ -18,11 +18,10 @@ deploymentTarget <- function(appPath = ".",
   if (nrow(appDeployments) == 0) {
     fullAccount <- findAccount(account, server)
     if (is.null(appName)) {
-      appName <- generateAppName(appTitle, appPath, account, unique = FALSE)
+      appName <- generateAppName(appTitle, recordPath, account, unique = FALSE)
     }
 
     createDeploymentTarget(
-      appPath,
       appName,
       appTitle %||% "",
       appId,
@@ -32,7 +31,6 @@ deploymentTarget <- function(appPath = ".",
     )
   } else if (nrow(appDeployments) == 1) {
     createDeploymentTarget(
-      appPath,
       appDeployments$name,
       appTitle %||% appDeployments$title,
       appDeployments$appId,
@@ -55,8 +53,7 @@ deploymentTarget <- function(appPath = ".",
   }
 }
 
-createDeploymentTarget <- function(appPath,
-                                   appName,
+createDeploymentTarget <- function(appName,
                                    appTitle,
                                    appId,
                                    username,
