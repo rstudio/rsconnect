rstudio/rsconnect

commit e1f5511e3459cc3b9a300cbb36561ed4e88f7a88

Refactor `bundlePackages()` (#670)
Turns the main strategy "inside out", so that we find all uninstalled packages, then all packages with missing repo, then collect the package metadata into a list. I've switched the error and warning to use cli and spent a little time polishing the language.

R/bundlePackage.R
@@ -3,7 +3,8 @@ bundlePackages <- function(appDir,
                            hasParameters = FALSE,
                            documentsHavePython = FALSE,
                            quartoInfo = NULL,
-                           verbose = FALSE
+                           verbose = FALSE,
+                           error_call = caller_env()
                            ) {
   if (appMode %in% c("static", "tensorflow-saved-model")) {
     return(list())
@@ -23,86 +24,52 @@ bundlePackages <- function(appDir,
     documentsHavePython = documentsHavePython
   )
   deps <- snapshotRDependencies(appDir, inferredRDependencies, verbose = verbose)
+  checkBundlePackages(deps, call = error_call)
 
-  packageMessages <- c()
-  errorMessages <- c()
-  packages <- list()
-
-  # construct package list from dependencies
-  for (i in seq_len(nrow(deps))) {
-    name <- deps[i, "Package"]
-
-    # get package info
-    info <- as.list(deps[i, c("Source", "Repository")])
-
-    # include github package info
-    info <- c(info, as.list(deps[i, grep("Github", colnames(deps), perl = TRUE, value = TRUE)]))
-
-    # get package description; note that we need to remove the
-    # packageDescription S3 class from the object or jsonlite will refuse to
-    # serialize it when building the manifest JSON
+  deps$description <- lapply(deps$Package, function(nm) {
+    # Remove packageDescription S3 class so jsonlite can serialize
     # TODO: should we get description from packrat/desc folder?
-    info$description <- suppressWarnings(unclass(utils::packageDescription(name)))
+    unclass(utils::packageDescription(nm))
+  })
 
-    # TODO(HW): should this code be removed? I don't see how it is ever reached
-    # if description is NA, application dependency may not be installed
-    if (is.na(info$description[1])) {
-      errorMessages <- c(
-        errorMessages,
-        paste0(
-          "Deployment depends on package \"", name, "\"",
-          "but it is not installed. Please resolve before continuing."
-        )
-      )
-      next
-    }
-
-    # validate package source (returns a message if there is a problem)
-    packageMessages <- c(packageMessages, validatePackageSource(deps[i, ]))
-
-    # good to go
-    packages[[name]] <- info
-  }
-
-  if (length(packageMessages)) {
-    # Advice to help resolve installed packages that are not available using the
-    # current set of configured repositories. Each package with a missing
-    # repository has already been printed (see snapshotDependencizes).
-    #
-    # This situation used to trigger an error (halting deployment), but was
-    # softened because:
-    #   * CRAN-archived packages are not visible to our available.packages
-    #     scanning.
-    #   * Source-installed packages may be available after a manual server-side
-    #     installation.
-    #
-    # That said, an incorrectly configured "repos" option is almost always the
-    # cause.
-    packageMessages <- c(
-      packageMessages,
-      paste0(
-        "Unable to determine the source location for some packages. ",
-        "Packages should be installed from a package repository like ",
-        "CRAN or a version control system. Check that ",
-        "options('repos') refers to a package repository containing ",
-        "the needed package versions."
-      )
-    )
-    warning(bullets(packageMessages), call. = FALSE, immediate. = TRUE)
-  }
-
-  if (length(errorMessages)) {
-    stop(bullets(errorMessages), call. = FALSE)
-  }
-
-  packages
+  # Connect prefers that packrat/packrat.lock file, but will use the manifest
+  # if needed. shinyapps.io only uses the manifest, and only supports Github
+  # remotes, not Bitbucket or Gitlab.
+  github_cols <- grep("Github", colnames(deps), perl = TRUE, value = TRUE)
+  packages <- deps[c("Source", "Repository", github_cols, "description")]
+  packages_list <- lapply(seq_len(nrow(packages)), function(i) {
+    as.list(packages[i, , drop = FALSE])
+  })
+  names(packages_list) <- deps$Package
+  packages_list
 }
 
-bullets <- function(x) {
-  bullets <- lapply(x, function(x) {
-    paste0(strwrap(x, initial = "* ", exdent = 2, width = 72), "\n", collapse = "")
-  })
-  paste0(bullets, "\n", collapse = "")
+checkBundlePackages <- function(deps, call = caller_env()) {
+  not_installed <- !vapply(deps$Package, is_installed, logical(1))
+  if (any(not_installed)) {
+    pkgs <- deps$Package[not_installed]
+    cli::cli_abort(
+      c(
+        "All packages used by the asset must be installed.",
+        x = "Missing packages: {.pkg {pkgs}}."
+      ),
+      call = call
+    )
+  }
+
+  unknown_source <- is.na(deps$Source)
+  if (any(unknown_source)) {
+    pkgs <- deps$Package[unknown_source]
+    cli::cli_warn(
+      c(
+        "Local packages must be installed from a supported source.",
+        x = "Unsupported packages: {.pkg {pkgs}}.",
+        i = "Supported sources are CRAN and CRAN-like repositories, BioConductor, GitHub, GitLab, and Bitbucket.",
+        i = "See {.fun rsconnect::appDependencies} for more details."
+      ),
+      call = call
+    )
+  }
 }
 
 ## check for extra dependencies uses
@@ -121,22 +88,6 @@ inferRPackageDependencies <- function(appMode,
     deps <- c(deps, "reticulate")
   }
   deps
-}
-
-validatePackageSource <- function(pkg) {
-  if (isSCMSource(pkg$Source)) {
-    return()
-  }
-
-  if (is.null(pkg$Repository) || is.na(pkg$Repository)) {
-    return(sprintf(
-      "May be unable to deploy package dependency '%s'; could not determine a repository URL for the source '%s'.",
-      pkg$Package,
-      pkg$Source
-    ))
-  }
-
-  return()
 }
 
 appUsesR <- function(quartoInfo) {

R/appDependencies.R
@@ -1,33 +1,53 @@
-#' Detect Application Dependencies
+#' Detect application dependencies
 #'
-#' Recursively detect all package dependencies for an application. This function
-#' parses all .R files in the application directory to determine what packages
-#' the application depends on; and for each of those packages what other
-#' packages they depend on.
+#' @description
+#' `appDependencies()` recursively detects all R package dependencies for an
+#' application by parsing all `.R` and `.Rmd` files and looking for calls
+#' to `library()`, `require()`, `requireNamespace()`, `::`, and so on.
+#' It then adds all recursive dependencies to create a complete manifest of
+#' package packages need to be installed to run the app.
+#'
+#' # Remote installation
+#'
+#' When deployed, the app must first install all of these packages, and
+#' rsconnect does its best to ensure that all the packages have the same
+#' version as you are running locally. It knows how to install packages from
+#' the following sources:
+#'
+#' * CRAN (`CRAN`) and CRAN-like repositories (`CustomCRANLikeRepository`).
+#' * BioConductor (`Bioconductor`)
+#' * Packages installed from GitHub (`github`), GitLab (`gitlab`), or
+#'   BitBucket (`bitbucket`).
+#'
+#' It does not know how to install packages that you have built and installed
+#' locally so if you attempt to deploy an app that depends on such a package
+#' it will fail. To resolve this issue, you'll need to install from a known
+#' source.
+#'
+#' # Suggested packages
+#'
+#' The `Suggests` field is not included when determining recursive dependencies,
+#' so it's possible that not every package required to run your application will
+#' be detected.
+#'
+#' For example, ggplot2's `geom_hex()` requires the hexbin package to be
+#' installed, but it is only suggested by ggplot2. So if you app uses
+#' `geom_hex()` it will fail, reporting that the hexbin package is not
+#' installed.
+#'
+#' You can overcome this problem with (e.g.) `requireNamespace(hexbin)`.
+#' This will tell rsconnect that your app needs the hexbin package, without
+#' otherwise affecting your code.
+#'
 #' @inheritParams deployApp
-#' @return Returns a data frame listing the package
-#'   dependencies detected for the application: \tabular{ll}{ `package`
-#'   \tab Name of package \cr `version` \tab Version of package\cr }
-#' @details Dependencies are determined by parsing application source code and
-#'   looking for calls to `library`, `require`, `::`, and
-#'   `:::`.
-#'
-#'   Recursive dependencies are detected by examining the `Depends`,
-#'   `Imports`, and `LinkingTo` fields of the packages immediately
-#'   dependend on by the application.
-#'
-#' @note Since the `Suggests` field is not included when determining
-#'   recursive dependencies of packages, it's possible that not every package
-#'   required to run your application will be detected.
-#'
-#'   In this case, you can force a package to be included dependency by
-#'   inserting call(s) to `require` within your source directory. This code
-#'   need not actually execute, for example you could create a standalone file
-#'   named `dependencies.R` with the following code: \cr \cr
-#'   `require(xts)` \cr `require(colorspace)` \cr
-#'
-#'   This will force the `xts` and `colorspace` packages to be
-#'   installed along with the rest of your application when it is deployed.
+#' @param appDir Directory containing application. Defaults to current working
+#'   directory.
+#' @return A data frame with columns:
+#'   * `Package`: package name.
+#'   * `Version`: local version.
+#'   * `Source`: where the package was installed from.
+#'   * `Repository`: for CRAN and CRAN-like repositories, the url to the
+#'      repo.
 #' @examples
 #' \dontrun{
 #'
@@ -45,5 +65,5 @@ appDependencies <- function(appDir = getwd(), appFiles = NULL) {
   on.exit(unlink(bundleDir, recursive = TRUE), add = TRUE)
 
   deps <- snapshotRDependencies(bundleDir)
-  deps[c("Package", "Version", "Source")]
+  deps[c("Package", "Version", "Source", "Repository")]
 }

R/bundle.R
@@ -353,6 +353,13 @@ snapshotRDependencies <- function(appDir, implicit_dependencies = c(), verbose =
     sapply(repos, "[[", 1)
   )
 
+  # get packages records defined in the lockfile
+  records <- utils::tail(df, -1)
+  records[c("Source", "Repository")] <- findPackageRepoAndSource(records, repos)
+  records
+}
+
+findPackageRepoAndSource <- function(records, repos) {
   # read available.packages filters (allow user to override if necessary;
   # this is primarily to allow debugging)
   #
@@ -361,10 +368,7 @@ snapshotRDependencies <- function(appDir, implicit_dependencies = c(), verbose =
   # in use can still be marked as available on CRAN -- for example, currently
   # the package "foreign" requires "R (>= 4.0.0)" but older versions of R
   # can still successfully install older versions from the CRAN archive
-  filters <- getOption(
-    "available_packages_filters",
-    default = c("duplicates")
-  )
+  filters <- getOption("available_packages_filters", default = "duplicates")
 
   # get Bioconductor repos if any
   biocRepos <- repos[grep("BioC", names(repos), perl = TRUE, value = TRUE)]
@@ -388,11 +392,11 @@ snapshotRDependencies <- function(appDir, implicit_dependencies = c(), verbose =
     name = names(named.repos),
     url = as.character(named.repos),
     contrib.url = contrib.url(named.repos, type = "source"),
-    stringsAsFactors = FALSE)
+    stringsAsFactors = FALSE
+  )
 
-  # get packages records defined in the lockfile
-  records <- utils::tail(df, -1)
-
+  # Sources are created by packrat:
+  # https://github.com/rstudio/packrat/blob/v0.9.0/R/pkg.R#L328
   # if the package is in a named CRAN-like repository capture it
   tmp <- lapply(seq_len(nrow(records)), function(i) {
 
@@ -428,10 +432,9 @@ snapshotRDependencies <- function(appDir, implicit_dependencies = c(), verbose =
       repository <- package.repo$url
     }
     # validatePackageSource will emit a warning for packages with NA repository.
-    data.frame(Source = source, Repository = repository)
+    data.frame(Source = source, Repository = repository, stringsAsFactors = FALSE)
   })
-  records[, c("Source", "Repository")] <- do.call("rbind", tmp)
-  return(records)
+  do.call("rbind", tmp)
 }
 
 addPackratSnapshot <- function(bundleDir, implicit_dependencies = c(), verbose = FALSE) {

man/appDependencies.Rd
@@ -2,7 +2,7 @@
 % Please edit documentation in R/appDependencies.R
 \name{appDependencies}
 \alias{appDependencies}
-\title{Detect Application Dependencies}
+\title{Detect application dependencies}
 \usage{
 appDependencies(appDir = getwd(), appFiles = NULL)
 }
@@ -16,39 +16,55 @@ files in \code{appDir}, apart from any listed in an \code{.rscignore} file.
 See \code{\link[=listBundleFiles]{listBundleFiles()}} for more details.}
 }
 \value{
-Returns a data frame listing the package
-dependencies detected for the application: \tabular{ll}{ \code{package}
-\tab Name of package \cr \code{version} \tab Version of package\cr }
+A data frame with columns:
+\itemize{
+\item \code{Package}: package name.
+\item \code{Version}: local version.
+\item \code{Source}: where the package was installed from.
+\item \code{Repository}: for CRAN and CRAN-like repositories, the url to the
+repo.
+}
 }
 \description{
-Recursively detect all package dependencies for an application. This function
-parses all .R files in the application directory to determine what packages
-the application depends on; and for each of those packages what other
-packages they depend on.
+\code{appDependencies()} recursively detects all R package dependencies for an
+application by parsing all \code{.R} and \code{.Rmd} files and looking for calls
+to \code{library()}, \code{require()}, \code{requireNamespace()}, \code{::}, and so on.
+It then adds all recursive dependencies to create a complete manifest of
+package packages need to be installed to run the app.
 }
-\details{
-Dependencies are determined by parsing application source code and
-looking for calls to \code{library}, \code{require}, \code{::}, and
-\code{:::}.
-
-Recursive dependencies are detected by examining the \code{Depends},
-\code{Imports}, and \code{LinkingTo} fields of the packages immediately
-dependend on by the application.
+\section{Remote installation}{
+When deployed, the app must first install all of these packages, and
+rsconnect does its best to ensure that all the packages have the same
+version as you are running locally. It knows how to install packages from
+the following sources:
+\itemize{
+\item CRAN (\code{CRAN}) and CRAN-like repositories (\code{CustomCRANLikeRepository}).
+\item BioConductor (\code{Bioconductor})
+\item Packages installed from GitHub (\code{github}), GitLab (\code{gitlab}), or
+BitBucket (\code{bitbucket}).
 }
-\note{
-Since the \code{Suggests} field is not included when determining
-recursive dependencies of packages, it's possible that not every package
-required to run your application will be detected.
 
-In this case, you can force a package to be included dependency by
-inserting call(s) to \code{require} within your source directory. This code
-need not actually execute, for example you could create a standalone file
-named \code{dependencies.R} with the following code: \cr \cr
-\code{require(xts)} \cr \code{require(colorspace)} \cr
-
-This will force the \code{xts} and \code{colorspace} packages to be
-installed along with the rest of your application when it is deployed.
+It does not know how to install packages that you have built and installed
+locally so if you attempt to deploy an app that depends on such a package
+it will fail. To resolve this issue, you'll need to install from a known
+source.
 }
+
+\section{Suggested packages}{
+The \code{Suggests} field is not included when determining recursive dependencies,
+so it's possible that not every package required to run your application will
+be detected.
+
+For example, ggplot2's \code{geom_hex()} requires the hexbin package to be
+installed, but it is only suggested by ggplot2. So if you app uses
+\code{geom_hex()} it will fail, reporting that the hexbin package is not
+installed.
+
+You can overcome this problem with (e.g.) \code{requireNamespace(hexbin)}.
+This will tell rsconnect that your app needs the hexbin package, without
+otherwise affecting your code.
+}
+
 \examples{
 \dontrun{
 

tests/testthat/test-bundlePackage.R
@@ -30,7 +30,8 @@ test_that("errors if dependencies aren't installed", {
     data.frame(
       Package = c("doesntexist1", "doesntexist2"),
       Source = "CRAN",
-      Repository = "https://cran.rstudio.com"
+      Repository = "https://cran.rstudio.com",
+      stringsAsFactors = FALSE
     )
   })
 
@@ -52,8 +53,9 @@ test_that("warns if can't find source", {
   mockr::local_mock(snapshotRDependencies = function(...) {
     data.frame(
       Package = "shiny",
-      Source = "foo",
-      Repository = NA
+      Source = NA,
+      Repository = NA,
+      stringsAsFactors = FALSE
     )
   })
 

commit c9939b9f1953a94f0d6ba87dbdfe8300d5c07228

Fix bug in bundlePackages()
Introduced in #670.

R/bundlePackage.R
@@ -38,7 +38,9 @@ bundlePackages <- function(appDir,
   github_cols <- grep("Github", colnames(deps), perl = TRUE, value = TRUE)
   packages <- deps[c("Source", "Repository", github_cols, "description")]
   packages_list <- lapply(seq_len(nrow(packages)), function(i) {
-    as.list(packages[i, , drop = FALSE])
+    out <- as.list(packages[i, , drop = FALSE])
+    out$description <- out$description[[1]]
+    out
   })
   names(packages_list) <- deps$Package
   packages_list