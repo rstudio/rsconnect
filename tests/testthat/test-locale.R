a9fc88bd2b53c37134c2c88e8f62c368aedb3d3e
Hadley Wickham <h.wickham@gmail.com>
2023-02-27 10:59:03 -0600

    Skip linux which uses C locale

--- tests/testthat/test-locale.R
+++ tests/testthat/test-locale.R
@@ -1,4 +1,6 @@
-test_that("locale is en_US on GHA", {
+test_that("locale is en_US on mac/windows GHA", {
+  skip_on_os("linux")
   skip_if_not(identical(Sys.getenv("GITHUB_ACTIONS"), "true"))
+
   expect_equal(detectLocale(), "en_US")
 })
