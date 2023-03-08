f0bbadb0d3c7a04e1fe879102441fb6dc229e1e1
Hadley Wickham <h.wickham@gmail.com>
2023-03-08 16:55:01 -0600

    Add test for libcurl PUT streaming

--- tests/testthat/test-http-libcurl.R
+++ tests/testthat/test-http-libcurl.R
@@ -7,3 +7,20 @@ test_that("basic HTTP methods work", {
   test_http_POST_file()
   test_http_headers()
 })
+
+test_that("can stream PUT content from disk", {
+  service <- httpbin_service()
+
+  path <- withr::local_tempfile(lines = c("1", "2", "3"))
+  resp <- PUT(
+    service,
+    authInfo = NULL,
+    path = "put",
+    contentType = "text/plain",
+    file = path
+  )
+  expect_equal(resp$status, 200)
+
+  contents <- handleResponse(resp)
+  expect_equal(contents$data, "1\n2\n3\n")
+})
