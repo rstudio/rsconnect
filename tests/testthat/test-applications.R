5edbbd51a89e47dc346fdcf3b52f676aacdf4fef
Hadley Wickham <h.wickham@gmail.com>
2023-03-16 10:28:55 -0500

    Add tests for synAppMetadata

---
+++ tests/testthat/test-applications.R
@@ -0,0 +1,36 @@
+test_that("syncAppMetadata updates deployment records", {
+  local_temp_config()
+  addTestServer()
+  addTestAccount("ron")
+
+  app <- local_temp_app()
+  addTestDeployment(app, appId = "123", metadata = list(when = 123))
+  mockr::local_mock(clientForAccount = function(...) {
+    list(
+      getApplication = function(...) list(title = "newtitle", url = "newurl")
+    )
+  })
+
+  syncAppMetadata(app)
+  deps <- deployments(app)
+  expect_equal(deps$title, "newtitle")
+  expect_equal(deps$url, "newurl")
+  expect_equal(deps$when, NULL)
+})
+
+test_that("syncAppMetadata deletes deployment records if needed", {
+  local_temp_config()
+  addTestServer()
+  addTestAccount("ron")
+
+  app <- local_temp_app()
+  addTestDeployment(app, appId = "123", metadata = list(when = 123))
+  mockr::local_mock(clientForAccount = function(...) {
+    list(
+      getApplication = function(...) abort(class = "rsconnect_http_404")
+    )
+  })
+
+  expect_snapshot(syncAppMetadata(app))
+  expect_equal(nrow(deployments(app)), 0)
+})
