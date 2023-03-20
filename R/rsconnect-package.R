477968296cfc4c82a5ecb89e6615387cecd99d2b
Hadley Wickham <hadley@posit.co>
2023-03-20 14:22:37 -0500

    Documentation tweaks (#784)
    * Eliminate `rsconnectPackages()` in favour of `appDependencies()`
    * Reduce `rsconnect-package` since that overall index is duplicated elsewhere
    * Lean roxygen2 features to generate more of package docs

--- R/rsconnect-package.R
+++ R/rsconnect-package.R
@@ -1,54 +1,11 @@
-#' Deployment Interface for R Markdown Documents and Shiny Applications
+#' @keywords internal
+"_PACKAGE"
+
+#' Using Packages with rsconnect
 #'
-#' The `rsconnect`` package provides a programmatic deployment
-#' interface for RPubs, shinyapps.io, and Posit Connect. Supported contents
-#' types include R Markdown documents, Shiny applications, plots, and static
-#' web content.
-#'
-#' @section Managing Applications:
-#'
-#' Deploy and manage applications with the following functions:
-#'
-#' * [deployApp()]:
-#' Deploy a Shiny application to a server.
-#'
-#' * [configureApp()]:
-#' Configure an application currently running on a server.
-#'
-#' * [restartApp()]:
-#' Restart an application currently running on a server.
-#'
-#' * [terminateApp()]:
-#' Terminate an application currently running on a server.
-#'
-#' * [deployments()]:
-#' List deployment records for a given application directory.
-#'
-#'
-#' More information on application management is available in the
-#' [applications()] help page.
-#'
-#' @section Managing Accounts and Users:
-#'
-#' Manage accounts on the local system.
-##
-#' * [setAccountInfo()]:
-#'   Register an account.
-#'
-#' * [removeAccount()]:
-#'   Remove an account.
-#'
-#' * [accountInfo()]:
-#'   View information for a given account.
-#'
-#' More information on account management is available in the
-#' [accounts()] help page.
-#'
-#' @name rsconnect-package
-#' @aliases rsconnect-package rsconnect
-#' @import rlang
-#' @docType package
-#' @keywords package
+#' See ?[appDependencies()]
+#' @keywords internal
+#' @name rsconnectPackages
 NULL
 
 #' HTTP Proxy Configuration
@@ -60,6 +17,7 @@ NULL
 NULL
 
 ## usethis namespace: start
+#' @import rlang
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
