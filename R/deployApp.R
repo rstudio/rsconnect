5ca1261b7eee503b3d793fe6ffbe8fba1f99d00e
Aron Atkins <aron@gweep.net>
2023-05-09 14:19:07 -0400

    type is not provided by all services
    fixes #847

--- R/deployApp.R
+++ R/deployApp.R
@@ -364,7 +364,7 @@ deployApp <- function(appDir = getwd(),
         application <- client$getApplication(target$appId, target$version)
         taskComplete(quiet, "Found application {.url {application$url}}")
 
-        if (application$type == "static") {
+        if (!is.null(application$type) && application$type == "static") {
           application$application_id <- client$createRevision(application)
         }
 
