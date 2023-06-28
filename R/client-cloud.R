1ccb992f323d4d7445451e5821e052efbec352a8
Matthew Lynch <matthew.lynch@posit.co>
2023-06-28 12:36:00 -0500

    Revert "fix indentation"
    This reverts commit fdbdada421995d783318e3639f47ae6024c592aa.

--- R/client-cloud.R
+++ R/client-cloud.R
@@ -100,7 +100,7 @@ cloudClient <- function(service, authInfo) {
 
             list(application = application, output = output)
           }
-        tryCatch(
+        applicationAndOutput <- tryCatch(
           {
             path <- paste0("/outputs/", outputOrApplicationId)
             output <- GET(service, authInfo, path)
@@ -113,6 +113,8 @@ cloudClient <- function(service, authInfo) {
           rsconnect_http_403 = handleError,
           rsconnect_http_404 = handleError
         )
+        application <- applicationAndOutput$application
+        output <- applicationAndOutput$output
       } else {
         # from dcf version >= 1, outputOrApplicationId is the id of the output.
         path <- paste0("/outputs/", outputOrApplicationId)
