53d410bf977732e4dc2aed41f066929898de5aa0
Aron Atkins <aron@gweep.net>
2023-08-16 12:05:07 -0400

    avoid zero-length files (#956)
    Debian discards zero-length files. We may add additional zero-length files
    within our test hierarchy in the future, but avoid them for now.
    
    See #954

---
+++ tests/testthat/shiny-app-in-subdir/my-app/server.R
@@ -0,0 +1,4 @@
+# Stub server.R
+#
+# Its presence (along with ui.R) indicates that this directory contains a
+# Shiny application.
---
+++ tests/testthat/shiny-app-in-subdir/my-app/ui.r
@@ -0,0 +1,4 @@
+# Stub ui.R
+#
+# Its presence (along with server.R) indicates that this directory contains a
+# Shiny application.
