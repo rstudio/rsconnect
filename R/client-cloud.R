bef4b0a3ebdfb1a27a63f052edbc5876aef5c821
Matthew Lynch <matthew.lynch@posit.co>
2023-09-11 11:59:18 -0500

    default contentCategory in createApplication to NULL
    Co-authored-by: Hadley Wickham <hadley@posit.co>

--- R/client-cloud.R
+++ R/client-cloud.R
@@ -161,7 +161,7 @@ cloudClient <- function(service, authInfo) {
       GET(service, authInfo, path, query)
     },
 
-    createApplication = function(name, title, template, accountId, appMode, contentCategory) {
+    createApplication = function(name, title, template, accountId, appMode, contentCategory = NULL) {
       json <- list()
       json$name <- name
       json$application_type <- if (appMode %in% c("rmd-static", "quarto-static", "static")) "static" else "connect"
