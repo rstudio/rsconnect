8d741d4de1fd8b4076ac05fc9807d086bbd8b41a
Aron Atkins <aron@gweep.net>
2024-10-21 09:23:21 -0400

    remove remaining layout check
    fixes #1102

--- tests/testthat/test-appMetadata.R
+++ tests/testthat/test-appMetadata.R
@@ -65,51 +65,6 @@ test_that("content type (appMode) is inferred and can be overridden", {
 
 # checkLayout -------------------------------------------------------------
 
-test_that("checkLayout() errors if primary doc & app.R", {
-  dir <- local_temp_app(list(
-    "app.R" = "",
-    "myscript.R" = ""
-  ))
-
-  expect_snapshot(checkAppLayout(dir, appPrimaryDoc = "myscript.R"), error = TRUE)
-})
-
-test_that("checkLayout succeeds with some common app structures", {
-  rmd <- local_temp_app(list("foo.Rmd" = ""))
-  expect_no_error(checkAppLayout(rmd))
-
-  qmd <- local_temp_app(list("foo.qmd" = ""))
-  expect_no_error(checkAppLayout(qmd))
-
-  # perhaps script.R is a pre-render script that generates *.qmd
-  project <- local_temp_app(list("_quarto.yml" = "", "script.R" = ""))
-  expect_no_error(checkAppLayout(project))
-
-  md <- local_temp_app(list("_quarto.yml" = "", "index.md" = ""))
-  expect_no_error(checkAppLayout(md))
-
-  shiny1 <- local_temp_app(list("app.R" = ""))
-  expect_no_error(checkAppLayout(shiny1))
-
-  shiny2 <- local_temp_app(list("server.R" = "", "ui.R" = ""))
-  expect_no_error(checkAppLayout(shiny2))
-
-  api <- local_temp_app(list("plumber.R" = ""))
-  expect_no_error(checkAppLayout(api))
-
-  static <- local_temp_app(list("foo.html" = ""))
-  expect_no_error(checkAppLayout(static))
-
-  staticxml <- local_temp_app(list("data.xml" = ""))
-  expect_no_error(checkAppLayout(staticxml))
-
-  staticdata <- local_temp_app(list(
-    "data.txt" = "",
-    "cats.csv" = ""
-  ))
-  expect_no_error(checkAppLayout(staticdata))
-})
-
 # inferAppMode ------------------------------------------------------------
 
 test_that("can infer mode for API with plumber.R", {
