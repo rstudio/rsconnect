c1fa590d06421436724af03a2e8b73774d61fb5d
Aron Atkins <aron@gweep.net>
2025-05-15 13:07:27 -0400

    head: declare utils import and use of utils::head (#1162)

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,3 @@
 #' @importFrom stats na.omit setNames
-#' @importFrom utils available.packages installed.packages contrib.url getFromNamespace glob2rx packageVersion packageDescription read.csv
+#' @importFrom utils available.packages installed.packages contrib.url getFromNamespace glob2rx head packageVersion packageDescription read.csv
 NULL
