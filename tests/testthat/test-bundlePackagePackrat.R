b2ef2e272f76af3d3836d90a83f081d251d247fd
Kara Woo <kara.woo@posit.co>
2025-06-04 16:44:24 -0700

    skip tests if suggested package isn't installed

--- tests/testthat/test-bundlePackagePackrat.R
+++ tests/testthat/test-bundlePackagePackrat.R
@@ -44,6 +44,7 @@ test_that("recommended packages are snapshotted", {
 test_that("works with BioC packages", {
   skip_on_cran()
   skip_on_ci()
+  skip_if_not_installed("Biobase")
   app <- local_temp_app(list(
     "index.Rmd" = c(
       "```{r}",
