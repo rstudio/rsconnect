7e3d690ba165892446ee303ee0b0848239e97339
joshyam-k <josh.yamamoto@posit.co>
2025-08-05 08:23:00 -0700

    camel snake camel snake

--- R/writeManifest.R
+++ R/writeManifest.R
@@ -73,7 +73,7 @@ writeManifest <- function(
     verbose = verbose,
     quiet = quiet
   )
-  manifest$integrationRequests <- list()
+  manifest$integration_requests <- list()
   manifestJson <- toJSON(manifest)
   manifestPath <- file.path(appDir, "manifest.json")
   writeLines(manifestJson, manifestPath, useBytes = TRUE)
