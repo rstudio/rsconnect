8960007853e4922603fcfefe649791ea7d11e4bb
Matthew Lynch <matthew.lynch@posit.co>
2025-10-01 16:58:35 -0400

    add clientForAccount test

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -4,6 +4,12 @@ test_that("shinyapps accounts create shinyapps clients", {
   expect_equal(client$service(), "shinyapps.io")
 })
 
+test_that("connect cloud accounts create connect cloud clients", {
+  account <- list(server = "connect.posit.cloud")
+  client <- clientForAccount(account)
+  expect_equal(client$service(), "connect.posit.cloud")
+})
+
 test_that("connect accounts create connect clients", {
   local_temp_config()
 
