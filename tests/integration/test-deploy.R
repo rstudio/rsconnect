c43374299fb0eb37689338fb8576643298197b9f
Neal Richardson <neal.p.richardson@gmail.com>
2025-12-13 10:22:19 -0500

    Test a little more

--- tests/integration/test-deploy.R
+++ tests/integration/test-deploy.R
@@ -1,3 +1,13 @@
 test_that("deploy does not error", {
-  expect_true(deployApp("example-shiny", account = account))
+  expect_true(deployApp("example-shiny", appTitle = "Test", account = account))
+})
+
+test_that("re-deploy does not error", {
+  # Let's exercise the env vars as well
+  expect_true(deployApp("example-shiny", envVars = "TEST", account = account))
+})
+
+test_that("listAccountEnvVars", {
+  envs <- listAccountEnvVars(account = account)
+  expect_true(nrow(envs) == 1)
 })
